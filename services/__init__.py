693d7ae389847824f65234508f7fc4650ce80e2a
gn00595067-rgb <gn00595067@gmail.com>
2026-03-06 15:40:13 +0800

    Update __init__.py 現價

--- services/__init__.py
+++ services/__init__.py
@@ -1,5 +1,17 @@
 # -*- coding: utf-8 -*-
-from .price_service import get_quote_cached, get_price_service
+from .price_service import (
+    get_quote_cached,
+    get_price_service,
+    fetch_stock_list_finmind,
+    fetch_stock_list_cached,
+)
 from .pnl_engine import compute_matches, Lot
 
-__all__ = ["get_quote_cached", "get_price_service", "compute_matches", "Lot"]
+__all__ = [
+    "get_quote_cached",
+    "get_price_service",
+    "fetch_stock_list_finmind",
+    "fetch_stock_list_cached",
+    "compute_matches",
+    "Lot",
+]
