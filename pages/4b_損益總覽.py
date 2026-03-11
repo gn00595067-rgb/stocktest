49ec9d07335a024b6c4dbf247e066fe812760bef
gn00595067-rgb <gn00595067@gmail.com>
2026-03-11 15:19:36 +0800

    ya

--- pages/4b_損益總覽.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""損益總覽：已與「投資績效」合併為一頁，自動導向合併頁。"""
-import streamlit as st
-
-st.set_page_config(page_title="損益總覽", layout="wide")
-# 與投資績效合併為「損益總覽與投資績效」，導向該頁
-st.switch_page("pages/4_投資績效.py")
